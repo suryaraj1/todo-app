6baa6d89cae8fb0771dd7d500a5b99c21ec0eb76
Suryaraj Bhaduri <suryaraj.bhaduri@yahoo.com>
2023-10-16 21:51:51 +0530

    Add simple server with go fiber

--- server/main.go
+++ server/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	fmt.Println("hello world")
+
+	app := fiber.New()
+
+	app.Get("/healthcheck", func(c *fiber.Ctx) error {
+		return c.SendString("I am alive..")
+	})
+
+	log.Fatal(app.Listen(":4000"))
 }
