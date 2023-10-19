295a72a22a2345bfeccdd7fc88a198c75ca78ed8
Suryaraj Bhaduri <suryaraj.bhaduri@yahoo.com>
2023-10-19 19:58:20 +0530

    Initial commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Box } from '@mantine/core'
+import useSWR from 'swr'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const PORT = 4000;
+export const ENDPOINT = `http://localhost:${PORT}`;
 
+const fetcher = (url: string) => fetch(`${ENDPOINT}/${url}`).then(response => response.json()) 
+
+function App() {
+
+  const { data, mutate } = useSWR("api/todos", fetcher);
+ 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <h1>{JSON.stringify(data)}</h1>
   )
 }
 
